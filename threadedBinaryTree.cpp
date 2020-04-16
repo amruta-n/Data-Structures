c04e9338c17b3aad8b59465b5c73104bf318bd32
Amruta Nandargi <55237212+amruta-n@users.noreply.github.com>
2020-04-16 22:38:01 +0530

    Add files via upload

---
+++ threadedBinaryTree.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+class Node
+{
+private:
+    bool lb=1, rb=1;
+    Node *right, *left;
+    char data;
+    friend class TBT;
+};
+
+class TBT
+{
+public:
+    Node *root;
+    TBT()
+    {
+        root = NULL;
+    }
+    void create(Node *root);
+    void createTree(Node *root);
+};
+
+void TBT :: create(Node *root)
+{
+    char ch;
+    cout<<"Enter root data:";
+    cin>>root->data;
+    root->left=root->right=NULL;
+    do
+    {   createTree(root);
+        cout<<"Do you want to continue?(Y/N)";
+        cin>>ch;
+    }while(ch=='y'|ch=='Y');
+}
+
+void TBT :: createTree(Node *root)
+{
+    char side;
+    Node *temp, *curr;
+    temp = root;
+    cout<<"\nWhere do you want to enter data?(L/R)";
+    cin>>side;
+    if(side == 'R' || side == 'r')
+    {
+        if(temp->right == NULL)
+        {
+            temp->rb=0;
+            curr = new Node;
+            temp->right = curr;
+            cout<<"\nEnter data:";
+            cin>>curr->data;
+            curr->left = curr->right =  NULL;
+        }
+        else{
+            createTree(temp->right);
+        }
+    }
+    else if(side == 'L' || side == 'l')
+    {
+        if(temp->left == NULL)
+        {
+            temp->lb=0;
+            curr = new Node;
+            temp->left = curr;
+            cout<<"\nEnter data:";
+            cin>>curr->data;
+            curr->left = curr->right =  NULL;
+        }
+        else{
+            createTree(temp->left);
+        }
+    }
+    else
+    {
+        cout<<"\nInvalid Entry";
+    }
+}
+
